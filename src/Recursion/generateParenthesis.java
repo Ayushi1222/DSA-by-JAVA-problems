a6e7a6f5bf766f814d5c1ed099450f50314cf194
Ayushi1222 <ayushikatroliya17h@gmail.com>
2024-01-17 12:00:53 +0530

    first commit

--- src/Recursion/generateParenthesis.java
+++ src/Recursion/generateParenthesis.java
@@ -2,6 +2,20 @@ package Recursion;
 
 public class generateParenthesis {
     public static void main(String[] args) {
+        int n=3;
+        generate(n,0,0,"");
+    }
+    public static void generate(int n,int open, int close, String ans)
+    {
+        if(open==n && close==n)
+        {
+            System.out.println(ans);
+            return;
+        }
+        if(close>open || open>n)
+            return;
+        generate(n,open+1,close, ans+"(");
+        generate(n,open,close+1,ans+")");
 
     }
 }
