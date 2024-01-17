95df44ca5029673573e515540c9c064f7da0bbb8
Ayushi1222 <ayushikatroliya17h@gmail.com>
2024-01-17 12:34:42 +0530

    first commit

---
+++ src/Recursion/printUniquePath.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class printUniquePath {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int m=s.nextInt();
+        int n=s.nextInt();
+        UniquePath(m,n,1,1,"");
+    }
+
+    public static void UniquePath(int m,int n,int row,int col,String ans)
+    {
+        if(row==m && col==n)
+        {
+            System.out.println(ans);
+            return;
+        }
+        if(row>m || col>n)
+            return;
+        UniquePath(m,n,row+1,col,ans+"V");
+        UniquePath(m,n,row,col+1,ans+"H");
+    }
+}
+
+
