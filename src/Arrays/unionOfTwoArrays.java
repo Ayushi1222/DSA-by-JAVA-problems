3431f6c65fc318e509a3eadb85506461c2a20847
Ayushi1222 <ayushikatroliya17h@gmail.com>
2024-02-14 10:49:46 +0530

    first commit

--- src/Arrays/unionOfTwoArrays.java
+++ src/Arrays/unionOfTwoArrays.java
@@ -7,28 +7,40 @@ public class unionOfTwoArrays {
         int[] a = {3, 3, 4, 5, 6, 7, 8, 9, 9, 9 };
         int[] b = {2 ,4 ,10 ,10 };
         List<Integer> ls = new ArrayList<>();
-        for (int i = 0; i < a.length; i++)
+        int i=0;
+        int j=0;
+//        int k=0;
+        while (i<a.length && j<b.length)
+        {
+            if(a[i]<b[j])
+            {
+                ls.add(a[i]);
+                i++;
+            }
+            else if(a[i]>b[j])
+            {
+                ls.add(b[j]);
+                j++;
+            }
+            else
+            {
+                i++;
+                j++;
+            }
+        }
+        while(i<a.length)
+        {
             ls.add(a[i]);
-        for (int i = 0; i < b.length; i++)
-            ls.add(b[i]);
-        Collections.sort(ls);
-        int k = 1;
-        int j = ls.size();
-        int num = ls.get(0);
-        int c = 1;
-        while (k < j) {
-            if (num == ls.get(k)) {
-                ls.remove(c+1);
-                num = ls.get(k+1);
-                k--;
-                j--;
-            }
-            else {
-                num = ls.get(k);
-            }
-
-            k++;
-            System.out.println(ls);
+            i++;
+        }
+        while(j<b.length)
+        {
+            ls.add(b[j]);
+            j++;
+        }
+        for(int x=0;x<ls.size();x++)
+        {
+            System.out.print(ls.get(x)+" ");
         }
     }
 }
