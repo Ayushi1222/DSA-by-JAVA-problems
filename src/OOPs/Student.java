8305ca6d6ee6c1d1c22efb819e938af2f5a9d8e2
Ayushi1222 <ayushikatroliya17h@gmail.com>
2024-02-21 11:23:47 +0530

    first commit

---
+++ src/OOPs/Student.java
@@ -0,0 +1,10 @@
+package OOPs;
+
+public class Student {
+    String name;
+    int age;
+    public void IntroduceYourself()
+    {
+        System.out.println("My name is "+name+ " and age is "+age);
+    }
+}
---
+++ src/OOPs/Student_client.java
@@ -0,0 +1,17 @@
+package OOPs;
+
+public class Student_client {
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.name ="Jigglypuff";
+        s.age=91;
+        System.out.println(s.name);
+        System.out.println(s.age);
+        s.IntroduceYourself();
+        Student s1=new Student();
+        s1.name="Pikachu";
+        s1.age=300;
+        s1.IntroduceYourself();
+
+    }
+}
