676f3bfd1745d8406cb78219a64d1e926c47df8c
Ayushi1222 <ayushikatroliya17h@gmail.com>
2024-02-21 12:23:17 +0530

    first commit

--- src/OOPs/Student.java
+++ src/OOPs/Student.java
@@ -7,4 +7,21 @@ public class Student {
     {
         System.out.println("My name is "+name+ " and age is "+age);
     }
+
+    public void SayHello(String name)
+    {
+        System.out.println(this.name+ " says hello to "+name);
+    }
+
+    //common for all then we will make it static function
+    public static void mentor(String name)
+    {
+        System.out.println("Mentor name is xyz "+name);
+    }
+
+    //Static block is independent of any object and will run at first and before anything else.
+    static
+    {
+        System.out.println("I am static block");
+    }
 }
--- src/OOPs/Student_client.java
+++ src/OOPs/Student_client.java
@@ -12,10 +12,12 @@ public class Student_client {
         s1.name="Pikachu";
         s1.age=300;
         s1.IntroduceYourself();
-        Student s3=new Student();
-        s3.name="Chikki chikki chikki chikki";
-        s3.age=300;
-        s3.IntroduceYourself();
+        Student s2=new Student();
+        s2.name="Chikki chikki chikki chikki";
+        s2.age=300;
+        s2.IntroduceYourself();
+        s.SayHello(s1.name);
+        Student.mentor(s2.name);
 
     }
 }
