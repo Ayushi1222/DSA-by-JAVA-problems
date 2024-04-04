8bfdb78c035987df0ce66204523bbe0ec33965cb
Ayushi1222 <ayushikatroliya17h@gmail.com>
2024-04-04 08:02:09 +0530

    Stack and Queue

---
+++ src/StackNdQueues/Queue_Client.java
@@ -0,0 +1,16 @@
+package StackNdQueues;
+
+public class Queue_Client {
+    public static void main(String[] args) throws Exception{
+        Queue q=new Queue();
+        q.Enqueue(10);
+        q.Enqueue(20);
+        q.Enqueue(30);
+        q.Enqueue(40);
+        q.Enqueue(50);
+        System.out.println(q.Dequeue());
+        System.out.println(q.Dequeue());
+        q.Enqueue(60);
+        q.Display();
+    }
+}
